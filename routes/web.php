e8639b38e851f29ed9b6c6023740a658d7679786
sangeethmsajan <sangeethmsajan123@gmail.com>
2026-03-18 19:14:19 +0530

    c
    c

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-use App\Models\User;
-
-Route::get('/test', function () {
-    $user = User::first();
-
-    activity()
-        ->performedOn(null)
-        ->log('User test on null object');
-
-    return 'done';
-});
